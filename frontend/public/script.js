7c4dc926b327d78758761511b89106d3ce0ee93e
kovacsbencet <kovacsbencet@gmail.com>
2022-04-06 16:19:33 +0200

    .

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -3,25 +3,37 @@ const parseJSON = async (url) => {
     return response.json();
 };
 
-const swiperComponent = () =>{
+const swiperComponent = (data, comp) =>{
     return `
     <div class="swiper">
         <div class="swiper-wrapper">
-            <div class="swiper-slide">Slide 1</div>
-            <div class="swiper-slide">Slide 2</div>
-            <div class="swiper-slide">Slide 3</div>
+            ${data.map(img => comp(img)).join("")}
         </div>
     </div>
     `
 };
 
+const swiperSlideComponent = ({filename, title}) => {
+    return `
+        <div class="swiper-slide"
+            <h2>${title}</h2>
+            <img src="/public/img/${filename}">
+        <div>
+    
+    `
+}
 
 const loadEvent = async () => {
     const rootElement = document.getElementById("root");
     const result = await parseJSON("/image-list");
 
-    rootElement.insertAdjacentHTML("beforeend", swiperComponent());
+    rootElement.insertAdjacentHTML("beforeend", swiperComponent(result, swiperSlideComponent));
 
-    const swiper = new Swiper(".swiper");
+    const swiper = new Swiper(".swiper", {
+        loop: true,
+    });
+
+
+    
 };
 window.addEventListener("load", loadEvent)
